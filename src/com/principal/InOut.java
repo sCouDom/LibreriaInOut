db05940f8e9c06dfeaf1bf9290a72671ecc2dea3
Sergio Counago Dominguez <scounagodominguez@danielcastelao.org>
2021-02-19 10:04:14 +0100

    nuevo método añadido

--- src/com/principal/InOut.java
+++ src/com/principal/InOut.java
@@ -1,7 +1,12 @@
 package com.principal;
 
+import javax.swing.*;
+
 public class InOut {
-    public void escribir(String msg){
+    public static void escribirConsola(String msg){
         System.out.println(msg);
+    }
+    public static void escribirVentana(String msg){
+        JOptionPane.showMessageDialog(null, msg);
     }
 }
